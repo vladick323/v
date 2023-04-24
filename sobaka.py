06cc234ef464a3b4cc327bc29b34e1b7dee94b04
admin <valugey09@gmail.com>
2023-04-24 14:52:45 +0300

    dz

--- sobaka.py
+++ sobaka.py
@@ -1,10 +1,12 @@
 import random
 class Human:
-    def __init__(self,name='None', eat=None, home=None, Owner=None, ):
+    def __init__(self,name='None', eat=None, home=None, Owner=None,
+naughty=None ):
         self.name="Dog"
         self.eat ="korm"
         self.home = "HouseDog"
         self.Owner = "Vlad"
+        self.naughty = "5"
 
     def get_name(self):
         pass
@@ -14,3 +16,8 @@ class Human:
         pass
     def get_Owner(self):
         pass
+    def get_naughty(self):
+        pass
+
+    if get_naughty == 5:
+        print("пропав хазяїн")
