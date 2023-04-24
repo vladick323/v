62bda69c8f72184e81a94ec2f3f22cbfea6d81a5
admin <valugey09@gmail.com>
2023-04-24 17:37:19 +0300

    initial commit

--- sims.py
+++ sims.py
@@ -9,17 +9,56 @@ class Human:
         self.home=home
         self.car=car
     def gey_home(self):
-        pass
+        self.home=House
     def gey_job(self):
-        pass
+        if self.car.drive():
+            pass
+        else:
+            self.to_repair()
+        return
+        self.job=Job(job_list)
     def gey_car(self):
-        pass
+        self.car=Auto(brand_of_car)
     def eat(self):
-        pass
+      if  self.home.food<=0:
+          self.shopping("food")
+      else:
+          if self.satiety>=100:
+              self.satiety=100
+              return
+            self.satiety+=5
+            self.home.food-=5
     def work(self):
-        pass
+        if self.car.drive():
+            pass
+        else:
+            if self.car.fuel<20:
+                self.shopping("fuel")
+                return
+            self.money+=self.job.salary
+            self.gladness-=self.job.gladness_less
+            self.satiety-=4
     def shopping(self,manage):
+
+
+    if self.car.drive():
         pass
+    else:
+        if self.car.fuel < 20:
+            manage=="fuel"
+              print("kupi palne")
+            self.money-=100
+            self.car.fuel+=100
+        elif manage=="food"
+            print("kypi igy")
+            self.money-=50
+            self.home.foos+=50
+        elif manage=="delecacies"
+            print("ehf cvfrjkbrb")
+            self.gladness+=10
+            self.satiety+=2
+            self.money+=15
+
     def chill(self):
         pass
     def clean_home(self):
@@ -39,9 +78,37 @@ class Auto:
         self.strenght=brand_list[self.brand]["strenght"]
         self.consuption=brand_list[self.brand]["consunption"]
 
+    def drive(self):
+        if self.strenght>0 and self.fuel>=self.consuption:
+           self.fuel-=self.consuption
+           self.strenght-=1
+            return True
+        else:
+            print("машина не може рухатися")
+            return False
+
+
+
+
+class House:
+    def __int__(self):
+        self.mess=0
+        self.food=0
 
         brand_of_car={
             "BMW":{"fuel":100, "strenght":100,"consumption":6},
             "Lada": {"fuel": 50, "strenght": 40, "consumption": 10},
             "Volvo": {"fuel": 70, "strenght": 150, "consumption": 8},
             "Ferrari": {"fuel": 80, "strenght": 120, "consumption": 14}}
+
+class Job:
+    def __init__(self, job_list):
+        self.job=random.choise(list(job_list))
+        self.salary=job_list[self.job]["salary"]
+        self.gladnes_less=job_list[self.job]["gladness_less"]
+
+    job_list={
+        "Java developer":{"salary":50, "gladness_less":10},
+        "Python developer": {"salary": 40, "gladness_less": 3},
+        "C++ developer": {"salary": 45, "gladness_less": 25},
+        "Rust developer": {"salary": 70, "gladness_less": 1}}
