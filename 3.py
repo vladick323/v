e69b5fdf6036769fae8094e2c33a5b2dbc7a556c
admin <valugey09@gmail.com>
2023-05-15 15:15:55 +0300

    initial commit

--- 3.py
+++ 3.py
@@ -5,15 +5,15 @@
 
 import requests
 
-response=requests.get("https://coinmarketcap.com/")
+response=requests.get("https://minfin.com.ua/currency/usd/")
 response_text=response.text
 response_text=response.text
-response_parse=response_text.split("<span>")
+response_parse=response_text.split("</div>")
 print(response_text)
 for parse_elem_1 in response_parse:
-    if parse_elem_1.startswith("$"):
-        for parse_elem_2 in parse_elem_1.split("</span>"):
-            if parse_elem_2.startswith("$") and parse_elem_2[1].isdigit():
+    if parse_elem_1.startswith("₴"):
+        for parse_elem_2 in parse_elem_1.split("</div>"):
+            if parse_elem_2.startswith("₴") and parse_elem_2[1].isdigit():
                 res_parse_list.append(parse_elem_2)
 bitcoin_exchange_rate = res_parse_list[0]
-print(bitcoin_exchange_rate)
+print(dollar_exchange_rate)
